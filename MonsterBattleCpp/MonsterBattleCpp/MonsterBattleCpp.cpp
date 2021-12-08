787b7b6325adcd37bec4dc132a1bc174da2e8db7
中村 隆徳 <t-nakamura@bns-g.com>
2021-12-08 18:19:48 +0900

    C++版のひな型作成

--- MonsterBattleCpp/MonsterBattleCpp/MonsterBattleCpp.cpp
+++ MonsterBattleCpp/MonsterBattleCpp/MonsterBattleCpp.cpp
@@ -1,12 +1,9 @@
 ﻿#include <vector>
 #include <algorithm>
-#include <iostream>
 #include <Windows.h>
 
-#include "MonsterData_generated.h"
-#include "flatbuffers/util.h"
 
-
+// FlatBuffersを使う段階になったら、この値を 1 (0以外) にしよう！
 #define USE_FLATBUFFERS 0 
 
 
@@ -23,6 +20,7 @@ struct MonsterData
 /// モンスターリスト
 static constexpr MonsterData s_monsterList[] =
 {
+	// label         name           hp      ap      dp
 	{ "demon",      "デーモン　", 	800,    40, 	25  },
 	{ "dragon", 	"ドラゴン　", 	900,	45, 	10  },
 	{ "ghost",  	"ゴースト　", 	500,    20, 	25  },
@@ -46,93 +44,36 @@ static constexpr MonsterData s_monsterList[] =
 // 2体は同時に戦います。同時に力尽きた時は、力尽きた時の体力が大きい方が勝ちです。（例：残り体力が -20 と -5になった場合は、-5の方が勝ち）
 
 
-void setCursorPos(int x, int y)
-{
-	HANDLE hCons = GetStdHandle(STD_OUTPUT_HANDLE);
-	COORD pos{ pos.X = x, pos.Y = y };
-	SetConsoleCursorPosition(hCons, pos);
-}
-
-void Attack(MonsterData& offense, MonsterData& defense, int y, int turn)
-{
-	int damage = offense.ap * (100 - defense.dp) / 100;
-	defense.hp -= damage;
-
-	//setCursorPos(0, 1);
-	//printf("TURN %d", turn);
-	//setCursorPos(0, y);
-	//const char* text = "  %10s の攻撃！ %10s に %3d のダメージ！ 残りHP(%4d)\n";
-	//printf(text, offense.name, defense.name, damage, defense.hp);
-	//printf("  %10s の攻撃！ %10s に %3d のダメージ！ 残りHP(%4d)\n", offense.name, defense.name, damage, defense.hp);
-}
-
-bool IsStronger(MonsterData& a, MonsterData& b)
-{
-	int turn = 1;
-	while (a.hp > 0 && b.hp > 0)
-	{
-		Attack(a, b, 2, turn);
-		Attack(b, a, 3, turn);
-		++turn;
-	}
-	//(void)getchar();
-
-	return a.hp >= b.hp;
-}
-
-bool Battle(const MonsterData& a, const MonsterData& b)
-{
-	//setCursorPos(0, 0);
-	//printf("%s vs %s\n", a.name, b.name);
-	//std::cout << a.name << " vs " << b.name << std::endl;
-
-	auto aa = a;
-	auto bb = b;
-	return IsStronger(aa, bb);
-}
-
 int main()
 {
-#if defined(USE_FLATBUFFERS) && USE_FLATBUFFERS
-	// flatbuffersでバイナリにシリアライズされたデータを読み込んで、monsterListを構築する
-	std::vector<MonsterData> monsterList;
-	std::string binaryData;
-	if (flatbuffers::LoadFile("ToolsAndData/MonsterData.mdfb", true, &binaryData))
-	{
-		auto monsters = Data::GetMonsterList(binaryData.data());
-		MonsterData monster;
-		for (const auto& it : *monsters->monster_list())
-		{
-			monster.label = it->label()->c_str();
-			monster.name = it->name()->c_str();
-			monster.hp = it->hp();
-			monster.ap = it->ap();
-			monster.dp = it->dp();
-			monsterList.push_back(monster);
-		}
-	}
-#else
-	std::vector<MonsterData> monsterList(std::begin(s_monsterList), std::end(s_monsterList));
-#endif
-
 	// コードページ(文字コード)を UTF-8 にするおまじない
+	// Windows10のバージョンによっては文字化けすることがあるので、文字化けするようならこれをコメントアウトしてみてください。
 	//SetConsoleOutputCP(65001);
 
 
+#if defined(USE_FLATBUFFERS) && USE_FLATBUFFERS
+	// FlatBuffersを使う段階になったら、ここにバイナリにシリアライズされたデータを読み込んで、 monsterList を構築する処理を実装しよう！
+	std::vector<MonsterData> monsterList;
+
+#else
+	// FlatBuffersを使わない段階では、ココで monsterList が構築される。
+	std::vector<MonsterData> monsterList(std::begin(s_monsterList), std::end(s_monsterList));
+
+#endif
+
 	// ソート前の状態を表示
-	//setCursorPos(0, 5);
 	for (const auto& monster : monsterList)
 	{
 		printf("%-10s : HP:%3d, ATK:%2d, DEF:%2d\n", monster.name, monster.hp, monster.ap, monster.dp);
 	}
 	printf("\n");
 
-	// ソート
-	std::sort(monsterList.begin(), monsterList.end(), [](const auto& a, const auto& b) { return Battle(a, b); });
+	// monsterList を強い順に並べ替える処理をココに実装しよう！
+	{
 
+	}
 
 	// ソート後の状態を表示
-	//setCursorPos(0, 14);
 	for (const auto& monster : monsterList)
 	{
 		printf("%-10s : HP:%3d, ATK:%2d, DEF:%2d\n", monster.name, monster.hp, monster.ap, monster.dp);
