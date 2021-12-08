67f7d68f6106bab8aa075a4e31ad2a49925b42ba
nakap3 <91584152+nakap3@users.noreply.github.com>
2021-12-08 16:49:02 +0900

    Add files via upload
    ツールと元データを追加

---
+++ ToolsAndData/MakeMonsterDataCpp.bat
@@ -0,0 +1,45 @@
+@echo off
+setlocal
+
+set _MANUAL=false
+set _IN_JSON=MonsterData.json
+set _IN_FBS=MonsterData.fbs
+set _OUT_PATH=..\MonsterBattleCpp\MonsterBattleCpp
+set _OUT_BIN=%_IN_JSON:json=mdfb%
+
+@rem ビルドツールから呼ぶ時は引数を設定する(引数自体には意味がない)
+@rem つまり、引数が無い時は手動でコンバートしている時
+
+if "%1" neq "" goto begin
+set _MANUAL=true
+
+
+:begin
+
+@rem カレントディレクトリをこのファイルがあるディレクトリに移動
+cd /D %~dp0
+
+@rem C++用のヘッダファイルを生成する
+flatc.exe --cpp -o %_OUT_PATH% %_IN_FBS%
+if %errorlevel% neq 0 goto error1
+
+@rem JSON化されたモンスターデータをバイナリにシリアライズする
+flatc.exe --unknown-json -b -o %_OUT_PATH% %_IN_FBS% %_IN_JSON%
+if %errorlevel% neq 0 goto error2
+
+echo 変換完了！ %_IN_JSON% with %_IN_FBS% ⇒ %_OUT_BIN%
+goto end
+
+
+:error1
+echo FlatBuffersの定義ファイル出力過程でエラーがあります
+goto end
+
+:error2
+echo FlatBuffersのワイヤーデータ出力過程でエラーがあります
+goto end
+
+
+:end
+if "%_MANUAL%" equ "true" pause
+exit /b %errorlevel%
